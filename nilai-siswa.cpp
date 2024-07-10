57c1dbeb3e6c927749cf23cd700f014c2b0726ef
samuelmahesasinulingga <sinulingga848@gmail.com>
2024-07-10 18:43:32 +0700

    second commit

--- nilai-siswa.cpp
+++ nilai-siswa.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-    int a[] = {80,90,70,80,100,60,60,50,75,95};
+    int a[] = {80,50,70,20,10,40,60,50,75,95};
     int length = sizeof(a)/ sizeof(a[0]);
 
     cout << "Data Nilai siswa yaitu : " << endl;
